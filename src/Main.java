c847be33f85a10d2b3214d20b5522d58ed1b83bb
Vasiliy <pilaroiding@gmail.com>
2022-12-15 17:45:36 +0300

    Output changed

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args)
     {
         HelloWorld helloWorld = new HelloWorld();
-        System.out.println("Hello world!");
+        System.out.println("Привет мир");
     }
 
     public static class HelloWorld
