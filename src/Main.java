e4878dc492aaaddc34baf1c8d8ea7faf360b12ea
Vasiliy <pilaroiding@gmail.com>
2022-12-15 17:40:27 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args)
     {
         HelloWorld helloWorld = new HelloWorld();
+        System.out.println("Hello world!");
     }
 
     public static class HelloWorld
